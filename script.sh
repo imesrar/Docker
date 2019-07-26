9424d794c23674164b3b4f013cbebe8e5f1e0267
imesrar <44215570+imesrar@users.noreply.github.com>
2019-07-26 15:33:03 +0100

    script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Port 2222" >> /etc/ssh/ssh_config
+cat gitlab.rb > /etc/gitlab/gitlab.rb
+service ssh restart
+(/opt/gitlab/embedded/bin/runsvdir-start &) && gitlab-ctl reconfigure && tail -f /dev/null
